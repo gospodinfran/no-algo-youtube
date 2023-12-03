88ed4455420c1b5dde3bb6818debc4411576b28b
Fran <kezicfran@gmail.com>
2023-12-03 22:53:07 +0100

    init

--- background.js
+++ background.js
@@ -1,32 +1,31 @@
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.action.setBadgeText({
-      text: 'OFF'
-    })
-  })
+  chrome.action.chrome.action.setBadgeText({
+    text: 'OFF',
+  });
+});
 
-  const opgg = 'https://www.op.gg/'
+const yt_url = 'https://youtube.com/';
 
-  chrome.action.onClicked.addListener(async (tab) => {
-    if (tab.url.startsWith(opgg)) {
-      const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-      const nextState = prevState === 'ON' ? 'OFF' : 'ON';
-  
-      await chrome.action.setBadgeText({
-        tabId: tab.id,
-        text: nextState
-      })
+chrome.action.onClicked.addListener(async (tab) => {
+  if (tab.url.startsWith(yt_url)) {
+    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+    const nextState = prevState === 'ON' ? 'OFF' : 'ON';
 
-      if (nextState === 'ON') {
-        await chrome.scripting.insertCSS({
-            files: ['boosted.css'],
-            target: { tabId: tab.id }
-        })
-      } else if (nextState === 'OFF') {
-        await chrome.scripting.removeCSS({
-            files: ['boosted.css'],
-            target: { tabId: tab.id }
-        })
-      }
+    await chrome.action.setBadgeText({
+      tabId: tab.id,
+      text: nextState,
+    });
+
+    if (nextState === 'ON') {
+      await chrome.scripting.insertCSS({
+        files: ['injection.css'],
+        target: { tabId: tab.id },
+      });
+    } else if (nextState === 'OFF') {
+      await chrome.scripting.removeCSS({
+        files: ['injection.css'],
+        target: { tabId: tab.id },
+      });
     }
-})
-    
+  }
+});
