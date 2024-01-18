5c07e576cd8ced9e3786ca47761d77ead02a3e38
Fran <kezicfran@gmail.com>
2024-01-18 23:11:30 +0100

    Removed clutter

--- background.js
+++ background.js
@@ -1,31 +1,10 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.action.chrome.action.setBadgeText({
-    text: 'OFF',
-  });
-});
+const yt = 'https://www.youtube.com/';
 
-const yt_url = 'https://youtube.com/';
-
-chrome.action.onClicked.addListener(async (tab) => {
-  if (tab.url.startsWith(yt_url)) {
-    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-    const nextState = prevState === 'ON' ? 'OFF' : 'ON';
-
-    await chrome.action.setBadgeText({
-      tabId: tab.id,
-      text: nextState,
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (tab.url.startsWith(yt) && tab.active) {
+    chrome.scripting.insertCSS({
+      files: ['search.css'],
+      target: { tabId: tab.id },
     });
-
-    if (nextState === 'ON') {
-      await chrome.scripting.insertCSS({
-        files: ['injection.css'],
-        target: { tabId: tab.id },
-      });
-    } else if (nextState === 'OFF') {
-      await chrome.scripting.removeCSS({
-        files: ['injection.css'],
-        target: { tabId: tab.id },
-      });
-    }
   }
 });
